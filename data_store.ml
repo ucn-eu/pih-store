3899a2f8fc6bc286bbca158b1ed0d724dc3f9f04
Qi LI <liqi0425@gmail.com>
2016-07-03 18:01:16 +0100

    remove data_store's dependency on V1.CLOCK

--- data_store.ml
+++ data_store.ml
@@ -1,117 +1,109 @@
-open V1
 open Result
 
 module B = Base_store
 
-module Make (C: CLOCK) = struct
- let (>>=) = Lwt.bind
- let return = Lwt.return
+let (>>=) = Lwt.bind
+let return = Lwt.return
 
- type t = {
-     store: B.t;
-     check: B.value -> bool;
- }
- type key = B.key
- type value = B.value
- type id = string
- type src = string * int
+type t = {
+    store: B.t;
+    time : unit -> string;
+    check: B.value -> bool;
+}
+type value = B.value
+type id = string
+type src = string * int
 
- let make ~owner ?check () =
-   B.init ~owner >>= fun store ->
-   let check = match check with
-     | Some c -> c | None -> fun _ -> true in
-   Lwt.return {store; check}
+let make ~owner ~time ?check () =
+  B.init ~owner >>= fun store ->
+  let check = match check with
+    | Some c -> c | None -> fun _ -> true in
+  Lwt.return {store; time; check}
 
- let data_root = "data"
- let log_root = "log"
+let data_root = "data"
+let log_root = "log"
 
- let t () = C.(
-   let t = time () |> gmtime in
-   Printf.sprintf "%d:%d:%d:%d:%d:%d"
-     t.tm_year t.tm_mon t.tm_mday t.tm_hour t.tm_min t.tm_sec)
-
- let with_log s ?src a k =
-   let src = match src with
-     | None -> "unknown"
-     | Some (ip, port) -> Printf.sprintf "%s:%d" ip port in
-   let time = t () in
-   k () >>= fun r ->
-   let re =
-     match r with
+let with_log s t ?src a k =
+  let src = match src with
+    | None -> "unknown"
+    | Some (ip, port) -> Printf.sprintf "%s:%d" ip port in
+  let time = t () in
+  k () >>= fun r ->
+  let re =
+    match r with
     | Ok _ -> "succeed"
     | Error e-> "fail: " ^ (Printexc.to_string e) in
-   let log =
-     Printf.sprintf "[%s] %s %s %s" time src a re in
-   B.create s ~check:(fun _ -> true) (log_root :: [time]) log
-   >>= fun _ -> Lwt.return r
+  let log =
+    Printf.sprintf "[%s] %s %s %s" time src a re in
+  B.create s ~check:(fun _ -> true) (log_root :: [time]) log
+  >>= fun _ -> Lwt.return r
 
- let read ?src {store; _} id =
-   let fn () = B.read store (data_root :: [id]) in
-   let action = "read " ^ id in
-   with_log store ?src action fn
+let read {store; time; _} ?src id =
+  let fn () = B.read store (data_root :: [id]) in
+  let action = "read " ^ id in
+  with_log store time ?src action fn
 
- let update ?src {store; check} id v =
-   let fn () = B.update store ~check (data_root :: [id]) v in
-   let action = Printf.sprintf "update %s to [%s]" id v in
-   with_log store ?src action fn
+let update {store; time; check} ?src id v =
+  let fn () = B.update store ~check (data_root :: [id]) v in
+  let action = Printf.sprintf "update %s to [%s]" id v in
+  with_log store time ?src action fn
 
- let create ?src {store; check} id v =
-   let fn () = B.create store ~check (data_root :: [id]) v in
-   let action = Printf.sprintf "create %s as [%s]" id v in
-   with_log store ?src action fn
+let create {store; time; check} ?src id v =
+  let fn () = B.create store ~check (data_root :: [id]) v in
+  let action = Printf.sprintf "create %s as [%s]" id v in
+  with_log store time ?src action fn
 
- let remove ?src {store; _} id =
-   let fn () = B.remove store (data_root :: [id]) in
-   let action = "remove " ^ id in
-   with_log store ?src action fn
+let remove {store; time; _} ?src id =
+  let fn () = B.remove store (data_root :: [id]) in
+  let action = "remove " ^ id in
+  with_log store time ?src action fn
 
- let list ?src {store; _} =
-   let parent = [data_root] in
-   let fn () =
-     B.list store ~parent () >>= function
-     | Ok lst ->
-        let id_of_k k = k |> List.rev |> List.hd in
-        let id_lst = List.map id_of_k lst in
-        return (Ok id_lst)
-     | Error _ as e -> return e
-   in
-   with_log store ?src "list data" fn
+let list {store; time; _} ?src () =
+  let parent = [data_root] in
+  let fn () =
+    B.list store ~parent () >>= function
+    | Ok lst ->
+       let id_of_k k = k |> List.rev |> List.hd in
+       let id_lst = List.map id_of_k lst in
+       return (Ok id_lst)
+    | Error _ as e -> return e
+  in
+  with_log store time ?src "list data" fn
 
- let get_meta ?src {store; _} id to_meta =
-   let fn () =
-     B.read store (data_root :: [id]) >>= function
-     | Ok v -> return (Ok (to_meta v))
-     | Error _ as e -> return e in
-   let action = "get metadata of " ^ id in
-   with_log store ?src action fn
+let get_meta {store;time;  _} ?src id to_meta =
+  let fn () =
+    B.read store (data_root :: [id]) >>= function
+    | Ok v -> return (Ok (to_meta v))
+    | Error _ as e -> return e in
+  let action = "get metadata of " ^ id in
+  with_log store time ?src action fn
 
- let compare x y =
-   let x = Astring.String.cuts ~empty:false ~sep:":" x in
-   let y = Astring.String.cuts ~empty:false ~sep:":" y in
-   let rec aux = function
-     | [], [] -> 0
-     | hx :: tx, hy :: ty ->
-        let c = Pervasives.compare hx hy in
-        if c = 0 then aux (tx, ty)
-        else c
-     | _ -> assert false in
-   aux (x, y)
+let compare x y =
+  let x = Astring.String.cuts ~empty:false ~sep:":" x in
+  let y = Astring.String.cuts ~empty:false ~sep:":" y in
+  let rec aux = function
+    | [], [] -> 0
+    | hx :: tx, hy :: ty ->
+       let c = Pervasives.compare hx hy in
+       if c = 0 then aux (tx, ty)
+       else c
+    | _ -> assert false in
+  aux (x, y)
 
- let list_logs ?src {store; _} =
-   let parent = [log_root] in
-   let fn () =
-     B.list store ~parent () >>= function
-     | Ok lst ->
-        let id_of_key = fun key -> key |> List.rev |> List.hd in
-        let id_lst = List.map id_of_key lst in
-        let sorted = List.sort compare id_lst in
-        return (Ok sorted)
-     | Error _ as e -> return e
-   in
-   with_log store ?src "list logs" fn
+let list_logs {store; time; _} ?src () =
+  let parent = [log_root] in
+  let fn () =
+    B.list store ~parent () >>= function
+    | Ok lst ->
+       let id_of_key = fun key -> key |> List.rev |> List.hd in
+       let id_lst = List.map id_of_key lst in
+       let sorted = List.sort compare id_lst in
+       return (Ok sorted)
+    | Error _ as e -> return e
+  in
+  with_log store time ?src "list logs" fn
 
- let read_log ?src {store; _} id =
-   let fn () = B.read store (log_root :: [id]) in
-   let action = "read log " ^ id in
-   with_log store ?src action fn
-end
+let read_log {store; time; _} ?src id =
+  let fn () = B.read store (log_root :: [id]) in
+  let action = "read log " ^ id in
+  with_log store time ?src action fn
--- data_store.mli
+++ data_store.mli
@@ -1,27 +1,27 @@
 open Result
 
-module Make(C : V1.CLOCK): sig
-  type t
-  type key = string list
-  type value = string
-  type src = string * int
-  type id = string
+type t
+type value = string
+type src = string * int
+type id = string
 
-  val make : owner:string -> ?check:(value -> bool) -> unit -> t Lwt.t
+val make :
+  owner:string ->
+  time:(unit -> string) ->
+  ?check:(value -> bool) -> unit -> t Lwt.t
 
-  val read : ?src:src -> t -> id -> (value, exn) result Lwt.t
+val read : t -> ?src:src -> id -> (value, exn) result Lwt.t
 
-  val update :?src:src -> t -> id -> value -> (unit, exn) result Lwt.t
+val update : t -> ?src:src -> id -> value -> (unit, exn) result Lwt.t
 
-  val create : ?src:src -> t -> id -> value -> (unit, exn) result Lwt.t
+val create : t -> ?src:src -> id -> value -> (unit, exn) result Lwt.t
 
-  val remove : ?src:src -> t -> id -> (unit, exn) result Lwt.t
+val remove : t -> ?src:src -> id -> (unit, exn) result Lwt.t
 
-  val list : ?src:src -> t -> (id list, exn) result Lwt.t
+val list : t -> ?src:src -> unit -> (id list, exn) result Lwt.t
 
-  val get_meta : ?src:src -> t -> id -> (value -> 'a) -> ('a, exn) result Lwt.t
+val get_meta : t -> ?src:src -> id -> (value -> 'a) -> ('a, exn) result Lwt.t
 
-  val list_logs : ?src:src -> t -> (string list, exn) result Lwt.t
+val list_logs : t -> ?src:src -> unit -> (string list, exn) result Lwt.t
 
-  val read_log : ?src:src -> t -> id -> (string, exn) result Lwt.t
-end
+val read_log : t -> ?src:src -> id -> (string, exn) result Lwt.t
