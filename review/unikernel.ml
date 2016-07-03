10ddaba23de91187528b209f022e5d298fda76bf
Qi LI <liqi0425@gmail.com>
2016-07-03 18:02:42 +0100

    pass in time() function rather than module of V1.CLOCK

--- review/unikernel.ml
+++ review/unikernel.ml
@@ -6,10 +6,9 @@ open Result
 let log_src = Logs.Src.create "ucn.review"
 module Log = (val Logs.src_log log_src : Logs.LOG)
 
-module Review_Store
-    (C:CLOCK) = struct
-  (*TODO: figure out build issue about data_store*)
-  module S = Data_store.Make(C)
+module Review_Store = struct
+
+  module S = Data_store
 
   let check s =
     try
@@ -36,35 +35,29 @@ module Review_Store
        return (Ok r)
     | Error _ as e -> return e
 
-  let dispatch ?src store body steps =
+  let dispatch store ?src body steps =
     match steps with
     | ["create"; id] ->
-       with_ok_unit (S.create ?src store id body)
+       with_ok_unit (S.create store ?src id body)
     | ["read"; id] ->
-       S.read ?src store id
+       S.read store ?src id
     | ["update"; id] ->
-       with_ok_unit (S.update ?src store id body)
+       with_ok_unit (S.update store ?src id body)
     | ["delete"; id] ->
-       with_ok_unit (S.remove ?src store id)
+       with_ok_unit (S.remove store ?src id)
     | ["meta"; id] ->
-       S.get_meta ?src store id to_meta
+       S.get_meta store ?src id to_meta
     | ["list"] ->
-       with_ok_list (S.list ?src store)
+       with_ok_list (S.list store ?src ())
     | _ -> return (Error Not_found)
 
-  let init () =
-    S.make ~owner:"ucn.review" ~check ()
+  let init ~time () =
+    S.make ~owner:"ucn.review" ~time ~check ()
 end
 
 
-module Main
-     (Http: Cohttp_lwt.Server)
-     (Keys: KV_RO)
-     (Clock: V1.CLOCK) = struct
-
-  module X509 = Tls_mirage.X509(Keys)(Clock)
-  module Store = Review_Store(Clock)
-  module Logs_reporter = Mirage_logs.Make(Clock)
+module Dispatcher
+    (Http: Cohttp_lwt.Server) = struct
 
   let headers = Cohttp.Header.init_with
     "Strict-Transport-Security" "max-age=31536000"
@@ -75,7 +68,7 @@ module Main
     let p = Uri.path uri in
     let steps = Astring.String.cuts ~empty:false ~sep:"/" p in
     Cohttp_lwt_body.to_string body >>= fun v ->
-    Store.dispatch ?src store v steps
+    Review_Store.dispatch store ?src v steps
     >>= function
     | Ok "" -> Http.respond ~headers ~status:`OK ~body:empty_body ()
     | Ok json ->
@@ -110,6 +103,22 @@ module Main
       Log.info (fun f -> f "[%s] closing" cid);
     in
     Http.make ~conn_closed ~callback ()
+end
+
+
+module Main
+     (Http: Cohttp_lwt.Server)
+     (Keys: KV_RO)
+     (Clock: V1.CLOCK) = struct
+
+  module X509 = Tls_mirage.X509(Keys)(Clock)
+  module Logs_reporter = Mirage_logs.Make(Clock)
+  module D = Dispatcher(Http)
+
+  let time () = Clock.(
+    let t = time () |> gmtime in
+    Printf.sprintf "%d:%d:%d:%d:%d:%d"
+      t.tm_year t.tm_mon t.tm_mday t.tm_hour t.tm_min t.tm_sec)
 
   let tls_init kv =
     X509.certificate kv `Default >>= fun cert ->
@@ -120,14 +129,14 @@ module Main
     Logs.(set_level (Some Info));
     Logs_reporter.(create () |> run) @@ fun () ->
 
-    Store.init () >>= fun s ->
+    Review_Store.init ~time () >>= fun s ->
 
     tls_init keys >>= fun cfg ->
     let tcp = `TCP 4433 in
     let tls = `TLS (cfg, tcp) in
 
     Lwt.join [
-      http tls @@ serve (review_dispatcher s);
-      http (`TCP 8081) @@ serve redirect
+      http tls @@ D.serve (D.review_dispatcher s);
+      http (`TCP 8081) @@ D.serve D.redirect
     ]
 end
